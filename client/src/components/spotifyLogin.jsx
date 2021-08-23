07ec5e10287a6be4a4cbca1af7162fe088008e30
Miguel Buenviaje <migbuen44@gmail.com>
2021-08-23 12:45:43 -0700

    Installed and implemented redux to handle user login logic. Installed and implemented react router to handle spotify login conditional page rendering

--- client/src/components/spotifyLogin.jsx
+++ client/src/components/spotifyLogin.jsx
@@ -3,7 +3,6 @@ import info from '../info';
 
 const SpotifyLogin = () => {
   const { AUTH_URL } = info;
-  console.log('spotify');
   return (
     <a className="spotifyLoginButton" href={AUTH_URL}>
       Spotify Login
