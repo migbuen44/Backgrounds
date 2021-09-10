d4b04ffc457712db75b915bfbb8e58b3d4e40616
Miguel Buenviaje <migbuen44@gmail.com>
2021-09-09 21:35:23 -0700

    Implemented basic pause and play functionality for images

--- client/src/components/imageContainer.jsx
+++ client/src/components/imageContainer.jsx
@@ -18,8 +18,11 @@ const ImageContainer = ({ search }) => {
   console.log('search: ', search);
   const [backgrounds, setBackgrounds] = useState([]);
   const [currentInterval, setCurrentInterval] = useState();
+  const [currentImageIdx, setCurrentImageIdx] = useState(0);
+  const [imagePaused, setImagePaused] = useState(false);
 
-  let currentImage = 0;
+  let imageIdx = 0;
+  console.log('currentImageIdx: ', currentImageIdx);
   const { pexelsAuth } = info;
 
   useEffect(() => {
@@ -33,35 +36,136 @@ const ImageContainer = ({ search }) => {
   }, [search]);
 
   const changeImg = () => {
+    console.log('imageIdx: ', imageIdx);
     if (!backgrounds.length) {
       return;
     }
 
-    document.body.style.backgroundImage = `url(${backgrounds[currentImage].src.landscape})`;
+    document.body.style.backgroundImage = `url(${backgrounds[imageIdx].src.landscape})`;
 
-    if (currentImage < backgrounds.length - 1) {
-      currentImage += 1;
+    if (imageIdx < backgrounds.length - 1) {
+      imageIdx += 1;
+      setCurrentImageIdx((prev) => prev + 1);
     } else {
-      currentImage = 0;
+      imageIdx = 0;
+      setCurrentImageIdx(0);
+    }
+  };
+
+  const playImages = () => {
+    if (currentInterval) {
+      clearInterval(currentInterval);
+    }
+    const slideShowTimer = setInterval(
+      changeImg.bind(imageIdx, currentImageIdx, setCurrentImageIdx), 3000,
+    );
+    setCurrentInterval(slideShowTimer);
+  };
+
+  const pauseImages = () => {
+    if (currentInterval) {
+      clearInterval(currentInterval);
     }
   };
 
   useEffect(() => {
+    setCurrentImageIdx(0);
     changeImg();
 
-    if (currentInterval) {
-      clearInterval(currentInterval);
-    }
-    const slideShowTimer = setInterval(changeImg.bind(currentImage), 3000);
-    setCurrentInterval(slideShowTimer);
+    playImages();
   }, [backgrounds]);
 
+  const toggle = () => {
+    if (imagePaused) {
+      imageIdx = currentImageIdx;
+      playImages();
+      setImagePaused(false);
+    } else {
+      pauseImages();
+      setImagePaused(true);
+    }
+  };
+
   return (
-    <div className="imageContainer">
-      {backgrounds.map((background, idx) =>
-        <Image key={idx} background={background} style={style} />)}
-    </div>
+    <>
+      <div onClick={toggle} style={{position: 'absolute', left: '30vw'}}>Toggle</div>
+      <div className="imageContainer">
+        {backgrounds.map((background, idx) =>
+          <Image key={idx} background={background} style={style} />)}
+      </div>
+    </>
   );
 };
 
 export default ImageContainer;
+
+// *******************
+
+// /* eslint-disable react/prop-types */
+// import React, { useState, useEffect } from 'react';
+// import axios from 'axios';
+// import info from '../info';
+// import Image from './image';
+
+// const style = {
+//   position: 'relative',
+//   right: '0.5vw',
+//   height: '70px',
+//   display: 'block',
+//   marginBottom: '-7px',
+//   alignSelf: 'center',
+// };
+
+// const ImageContainer = ({ search }) => {
+//   console.log('imagecontainer');
+//   console.log('search: ', search);
+//   const [backgrounds, setBackgrounds] = useState([]);
+//   const [currentInterval, setCurrentInterval] = useState();
+
+//   let currentImageIdx = 0;
+//   console.log('currentImageIdx: ', currentImageIdx);
+//   const { pexelsAuth } = info;
+
+//   useEffect(() => {
+//     if (search === '' || search === undefined) return;
+
+//     axios.get(`https://api.pexels.com/v1/search?query=${search}+wallpaper&per_page=70`, pexelsAuth)
+//       .then((data) => {
+//         const { photos } = data.data;
+//         setBackgrounds(photos);
+//       });
+//   }, [search]);
+
+//   const changeImg = () => {
+//     if (!backgrounds.length) {
+//       return;
+//     }
+
+//     document.body.style.backgroundImage = `url(${backgrounds[currentImageIdx].src.landscape})`;
+
+//     if (currentImageIdx < backgrounds.length - 1) {
+//       currentImageIdx += 1;
+//     } else {
+//       currentImageIdx = 0;
+//     }
+//   };
+
+//   useEffect(() => {
+//     changeImg();
+
+//     if (currentInterval) {
+//       clearInterval(currentInterval);
+//     }
+//     const slideShowTimer = setInterval(changeImg.bind(currentImageIdx), 3000);
+//     setCurrentInterval(slideShowTimer);
+//   }, [backgrounds]);
+
+//   return (
+//     <div className="imageContainer">
+//       {backgrounds.map((background, idx) =>
+//         <Image key={idx} background={background} style={style} />)}
+//     </div>
+//   );
+// };
+
+// export default ImageContainer;
