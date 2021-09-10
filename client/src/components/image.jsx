7624b850c5cb2fe1883576cb7fac579d99ea1c5f
Miguel Buenviaje <migbuen44@gmail.com>
2021-09-10 13:57:01 -0700

    Implemented feature that allows user to manually select which image to display from image container.

--- client/src/components/image.jsx
+++ client/src/components/image.jsx
@@ -1,14 +1,22 @@
 /* eslint-disable react/prop-types */
 import React, {useState, useEffect} from 'react';
 
-const Image = ({ background, style }) => {
+const Image = ({
+  background,
+  style,
+  setImageClickedIdx,
+  idx,
+}) => {
   // let [fullImageIsOpen, setFullImageIsOpen] = useState(false);
   const src = background.src.landscape;
 
+  const handleImageClick = () => {
+    setImageClickedIdx(idx);
+  };
+
   return (
     <>
-      <img style={style} src={src} alt="" />
-      &nbsp;
+      <img style={style} src={src} onClick={handleImageClick}alt="" />
     </>
   );
 };
