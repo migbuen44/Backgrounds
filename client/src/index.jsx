4d6e049c1b55575bdcbfb83e19ee9604921810d0
Miguel Buenviaje <migbuen44@gmail.com>
2021-10-13 14:43:48 -0700

    Deleted unnecessary client side files

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider} from 'react-redux';
-import { createStore } from 'redux';
-import rootReducer from './rootReducer';
+import { Provider } from 'react-redux';
 import App from './components/app';
 import store from './store';
 
--- client/src/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import loginReducer from './reducers/loginReducer';
-
-const rootReducer = combineReducers({
-  loginReducer,
-});
-
-export default rootReducer;
