5aedf854a7a0ae571d57955d7064d8c3a9fd5d6d
Miguel Buenviaje <migbuen44@gmail.com>
2021-10-14 19:33:24 -0700

    Created tests for models

--- server/models/index.js
+++ server/models/index.js
@@ -14,7 +14,6 @@ const db = {
     });
   },
   getUser: (email, callback) => {
-    console.log('email: ', email);
     const queryString = `SELECT * FROM users WHERE email = '${email}'`;
 
     pool.query(queryString, (err, result) => {
